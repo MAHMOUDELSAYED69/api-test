048b398aaa2684b6d98e71f2c73b9320dda4fbaa
MahmoudElSayed96 <gcfjxvkj@gmail.com>
2023-09-07 17:52:59 +0300

    m

--- app.js
+++ app.js
@@ -22,8 +22,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(cors({
   origin: '*',
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  exposedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
 }));
 
